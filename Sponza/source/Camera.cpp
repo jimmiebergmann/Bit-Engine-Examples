7b515ff059ddeac71b67d7496bf1e571b7435504
jimmiebergmann <jimmiebergmann@gmail.com>
2013-07-03 23:46:05 +0200

    Restored the sponza project
    Note: Can not run the Dynamic Debug profile

---
+++ Sponza/source/Camera.cpp
@@ -0,0 +1,237 @@
+// ///////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2013 Jimmie Bergmann - jimmiebergmann@gmail.com
+//
+// This software is provided 'as-is', without any express or
+// implied warranty. In no event will the authors be held
+// liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute
+// it freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but
+//    is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any
+//    source distribution.
+// ///////////////////////////////////////////////////////////////////////////
+
+#include <Camera.hpp>
+#include <iostream>
+#include <Bit/System/Debugger.hpp>
+#include <Bit/System/MemoryLeak.hpp>
+
+// Constructor
+Camera::Camera( ) :
+	m_Position( 0.0f, 0.0f, 0.0f ),
+	m_Direction( 0.0f, 0.0f, 1.0f ),
+	m_DirectionFlank( 1.0f, 0.0f, 0.0f ),
+	m_CameraAngles( 0.0f, 0.0f ),
+	m_MovementSpeed( 1.0f ),
+	m_EyeSpeed( 1.0f ),
+	m_UpdatePosition( BIT_FALSE ),
+	m_UpdateDirection( BIT_FALSE ),
+	m_NewMovementDirection( 0.0f, 0.0f, 0.0f ),
+	m_NewEyeDirection( 0.0f, 0.0f )
+{
+	CalculateDirectionFlank( );
+}
+
+
+// Public functions
+void Camera::MoveForwards( )
+{
+	m_UpdatePosition = BIT_TRUE;
+	m_NewMovementDirection += m_Direction;
+}
+
+void Camera::MoveBackwards( )
+{
+	m_UpdatePosition = BIT_TRUE;
+	m_NewMovementDirection -= m_Direction;
+}
+
+void Camera::MoveLeft( )
+{
+	m_UpdatePosition = BIT_TRUE;
+	m_NewMovementDirection -= m_DirectionFlank;
+}
+
+void Camera::MoveRight( )
+{
+	m_UpdatePosition = BIT_TRUE;
+	m_NewMovementDirection += m_DirectionFlank;
+}
+
+void Camera::RotateRight( BIT_FLOAT32 p_Amount )
+{
+	m_UpdateDirection = BIT_TRUE;
+	m_NewEyeDirection.y -= p_Amount;
+}
+
+void Camera::RotateLeft( BIT_FLOAT32 p_Amount )
+{
+	m_UpdateDirection = BIT_TRUE;
+	m_NewEyeDirection.y += p_Amount;
+}
+
+void Camera::RotateUp( BIT_FLOAT32 p_Amount )
+{
+	m_UpdateDirection = BIT_TRUE;
+	m_NewEyeDirection.x -= p_Amount;
+}
+
+void Camera::RotateDown( BIT_FLOAT32 p_Amount )
+{
+	m_UpdateDirection = BIT_TRUE;
+	m_NewEyeDirection.x += p_Amount;
+}
+
+
+BIT_BOOL Camera::Update( const BIT_FLOAT64 p_DeltaTime )
+{
+	// Update the position
+	if( m_UpdatePosition )
+	{
+		// Normalize the new direction
+		m_NewMovementDirection.Normalize( );
+
+		// Set the new position
+		m_Position += m_NewMovementDirection * m_MovementSpeed * p_DeltaTime;
+
+		// Reset the mew movement direction.
+		m_NewMovementDirection.x = 0.0f;
+		m_NewMovementDirection.y = 0.0f;
+		m_NewMovementDirection.z = 0.0f;
+	}
+
+	// Update the direction
+	if( m_UpdateDirection )
+	{
+		m_CameraAngles.x += m_NewEyeDirection.x * m_EyeSpeed * p_DeltaTime;
+		m_CameraAngles.y += m_NewEyeDirection.y * m_EyeSpeed * p_DeltaTime;
+
+		if( m_CameraAngles.x >= 89.99f ) m_CameraAngles.x = 89.99f;
+		if( m_CameraAngles.x <= -89.99f ) m_CameraAngles.x = -89.99f;
+
+		if( m_CameraAngles.y >= 360.0f ) m_CameraAngles.y -= 360.0f;
+		if( m_CameraAngles.y <= 0.0f ) m_CameraAngles.y += 360.0f;
+
+		// Update the direction flank
+		CalculateDirectionFlank( );
+
+		m_NewEyeDirection.x = 0.0f;
+		m_NewEyeDirection.y = 0.0f;	
+
+		m_Direction = Bit::Vector3_f32( 0.0f, 0.0f, 1.0f );
+		m_Direction.RotateX( -m_CameraAngles.x );
+		m_Direction.RotateY( -m_CameraAngles.y );
+	}
+
+	// Check if we actually changed the matrix
+	BIT_BOOL UpdateStatus = BIT_FALSE;
+
+	// Update the matrix
+	if( m_UpdatePosition || m_UpdateDirection )
+	{
+		UpdateStatus = BIT_TRUE;
+
+		// Update the matrix
+		UpdateMatrix( );
+	}
+
+	// Reset the position and direction update flag
+	m_UpdatePosition = BIT_FALSE;
+	m_UpdateDirection = BIT_FALSE;
+
+	return UpdateStatus;
+}
+
+void Camera::UpdateMatrix( )
+{
+	m_Matrix.Identity( );
+	m_Matrix.LookAt( m_Position, m_Position + m_Direction, Bit::Vector3_f32( 0.0f, 1.0f, 0.0f ) );
+	/*m_Matrix.Translate( -m_Position.x, -m_Position.y, -m_Position.z );
+	m_Matrix.RotateX( m_CameraAngles.x );
+	m_Matrix.RotateY( m_CameraAngles.y );
+	m_Matrix.Translate( m_Position.x, m_Position.y, m_Position.z );*/
+	
+}
+
+
+// Set functions
+void Camera::SetPosition( const Bit::Vector3_f32 p_Position )
+{
+	m_Position = p_Position;
+}
+
+void Camera::SetDirection( Bit::Vector3_f32 p_Direction )
+{
+	// normalize the direction
+	p_Direction.Normalize( );
+
+	// Calculate the Y angle ( vertical spinning, (360 degrees) )
+	m_CameraAngles.y = Bit::Vector3_f32::AngleBetweenVectors( Bit::Vector3_f32( p_Direction.x, 0.0f, p_Direction.y ), Bit::Vector3_f32( 0.0f, 1.0f, 0.0f ) );
+
+	// We have to fix the 0 to 180 degree period by adding 180.
+	if( p_Direction.x < 0.0f )
+	{
+		m_CameraAngles.y += 180.0f;
+	}
+
+	// Calculate the X angle ( camera up and down )
+	m_CameraAngles.x = Bit::Vector3_f32::AngleBetweenVectors( p_Direction, Bit::Vector3_f32( p_Direction.x, 0.0f, p_Direction.y ) );
+
+	if( p_Direction.y < 0.0f )
+	{
+		m_CameraAngles.x = -m_CameraAngles.x;
+	}
+
+
+
+
+	m_Direction = p_Direction;
+	CalculateDirectionFlank( );
+}
+
+void Camera::SetMovementSpeed( const BIT_FLOAT32 p_Speed )
+{
+	m_MovementSpeed = p_Speed;
+}
+void Camera::SetEyeSpeed( const BIT_FLOAT32 p_Speed )
+{
+	m_EyeSpeed = p_Speed;
+}
+
+// Get functions
+Bit::Matrix4x4 Camera::GetMatrix( ) const
+{
+	return m_Matrix;
+}
+
+Bit::Vector3_f32 Camera::GetPosition( ) const
+{
+	return m_Position;
+}
+
+Bit::Vector3_f32 Camera::GetDirection( ) const
+{
+	return m_Direction;
+}
+
+Bit::Vector2_f32 Camera::GetAngles( ) const
+{
+	return m_CameraAngles;
+}
+
+// Private functions
+void Camera::CalculateDirectionFlank( )
+{
+	m_DirectionFlank = m_Direction.Cross( Bit::Vector3_f32( 0.0f, 1.0f, 0.0f ) ).Normal( );
+}
---
+++ Sponza/source/Main.cpp
@@ -0,0 +1,652 @@
+#include <Bit/Window/Window.hpp>
+#include <Bit/Graphics/GraphicDevice.hpp>
+#include <Bit/Graphics/Image.hpp>
+#include <Bit/Graphics/ModelOBJ.hpp>
+#include <Bit/Graphics/ShaderProgram.hpp>
+#include <Bit/System/Timer.hpp>
+#include <Bit/System.hpp>
+#include <Bit/System/Vector3.hpp>
+#include <Bit/System/MatrixManager.hpp>
+#include <Bit/System/ResourceManager.hpp>
+#include <Bit/System/Debugger.hpp>
+#include <Bit/System/MemoryLeak.hpp>
+#include <Camera.hpp>
+
+// Window/graphic device
+Bit::Window * pWindow = BIT_NULL;
+Bit::GraphicDevice * pGraphicDevice = BIT_NULL;
+
+// Level variables
+const std::string LevelModelPath = "../../../../Sponza/sponza.obj";
+Bit::Model * pLevelModel = BIT_NULL;
+Bit::ShaderProgram * pShaderProgram_Model = BIT_NULL;
+Bit::Shader * pVertexShader_Model = BIT_NULL;
+Bit::Shader * pFragmentShader_Model = BIT_NULL;
+
+// Camera variables
+Camera Camera;
+Bit::Vector2_si32 MousePosition( 0, 0 );
+Bit::Vector2_si32 MouseLockPosition( 500, 500 );
+
+// Setting varialbes
+Bit::Vector2_ui32 WindowSize( 1600, 1000 );
+BIT_BOOL UseNormalMapping = BIT_TRUE;
+
+// Global functions
+int CloseApplication( const int p_Code );
+void InitializeMatrixManager( );
+void InitializeCamera( );
+BIT_UINT32 CreateWindow( );
+BIT_UINT32 CreateGraphicDevice( );
+BIT_UINT32 CreateModel( );
+BIT_UINT32 CreateModelShader( );
+
+// Main function
+int main( int argc, char ** argv )
+{
+	// Initialize the memory leak detector for Win32 only (ignored by default in linux)
+	bitInitMemoryLeak( BIT_NULL );
+
+	// Setting the absolute path in order to read files.
+	Bit::SetAbsolutePath( argv[ 0 ] );
+
+	// Initialize the matrix manager
+	InitializeMatrixManager( );
+
+	// Initialize the camera
+	InitializeCamera( );
+
+	// Initialize the application
+	if( CreateWindow( ) != BIT_OK ||
+		CreateGraphicDevice( ) != BIT_OK ||
+		CreateModel( ) != BIT_OK  ||
+		CreateModelShader( ) != BIT_OK )
+	{
+		return CloseApplication( 0 );
+	}
+
+	// Create a timer and run a main loop for some time
+	BIT_FLOAT64 DeltaTime = 0.0f;
+	Bit::Timer Timer;
+	Timer.Start( );
+
+	// Run the main loop
+	while( pWindow->IsOpen( ) )
+	{
+		// Get the delta time;
+		DeltaTime = Timer.GetLapsedTime( );
+		Timer.Start( );
+
+		// Do evenets
+		pWindow->Update( );
+
+		// Mouse lock
+		if( pWindow->IsFocused( ) )
+		{
+			MousePosition = pWindow->GetCursorScreenPosition(  );
+			pWindow->SetCursorPosition( MouseLockPosition );
+		}
+
+		Bit::Event Event;
+		while( pWindow->PollEvent( Event ) )
+		{
+			// Swtich the event type
+			switch( Event.Type )
+			{
+				case Bit::Event::Closed:
+				{
+					// Close the application
+					return CloseApplication( 0 );
+				}
+				break;
+                case Bit::Event::Moved:
+				{
+					continue;
+				}
+				break;
+				case Bit::Event::Resized:
+				{
+					continue;
+				}
+				break;
+				case Bit::Event::GainedFocus:
+				{
+					pWindow->ShowCursor( BIT_FALSE );
+					continue;
+				}
+				break;
+				case Bit::Event::LostFocus:
+				{
+					pWindow->ShowCursor( BIT_TRUE );
+					continue;
+				}
+				break;
+				case Bit::Event::KeyPressed:
+				{
+					switch( Event.Key )
+					{
+						// Movement keys
+						case 'W':
+						{
+							Camera.MoveForwards( );
+						}
+						break;
+						case 'S':
+						{
+							Camera.MoveBackwards( );
+						}
+						break;
+						case 'A':
+						{
+							Camera.MoveLeft( );
+						}
+						break;
+						case 'D':
+						{
+							Camera.MoveRight( );
+						}
+						break;
+						// Culling
+						case 'Z':
+						{
+							pGraphicDevice->EnableFaceCulling( Bit::GraphicDevice::Culling_BackFace );
+						}
+						break;
+						case 'X':
+						{
+							pGraphicDevice->EnableFaceCulling( Bit::GraphicDevice::Culling_FrontFace );
+						}
+						break;
+						case 'C':
+						{
+							pGraphicDevice->DisableFaceCulling( );
+						}
+						break;
+						// Mouse visibility
+						case 'V':
+						{
+							pWindow->ShowCursor( BIT_TRUE );
+						}
+						break;
+						case 'B':
+						{
+							pWindow->ShowCursor( BIT_FALSE );
+						}
+						break;
+						case 'M':
+						{
+							// Flip the flag
+							UseNormalMapping = !UseNormalMapping;
+							
+							// Bind and update the uniform
+							pShaderProgram_Model->Bind( );
+							pShaderProgram_Model->SetUniform1i( "UseNormalMapping", UseNormalMapping );
+							pShaderProgram_Model->Unbind( );
+						}
+						break;
+						
+						// Exit keys
+						case 27:
+						{
+							return CloseApplication( 0 );
+						}
+						break;
+						case 65307:
+						{
+							return CloseApplication( 0 );
+						}
+						break;
+					}
+
+				}
+				break;
+				case Bit::Event::KeyReleased:
+				{
+					bitTrace( "[Event] Key Released: %i\n", Event.Key );
+				}
+				break;
+				case Bit::Event::MouseMoved:
+				{
+					continue;
+				}
+				break;
+				case Bit::Event::MouseButtonPressed:
+				{
+					continue;
+				}
+				break;
+				case Bit::Event::MouseButtonReleased:
+				{
+					continue;
+				}
+				break;
+				default:
+					break;
+			}
+		}
+
+
+		// Update the camera angles
+		Bit::Vector2_f32 CameraDiffs;
+		CameraDiffs.x = (BIT_FLOAT32)MousePosition.x - (BIT_FLOAT32)MouseLockPosition.x;
+		CameraDiffs.y = (BIT_FLOAT32)MousePosition.y - (BIT_FLOAT32)MouseLockPosition.y;
+		if( CameraDiffs.y > 0.0f )
+		{
+			Camera.RotateUp( abs( CameraDiffs.y ) );
+		}
+		else if( CameraDiffs.y < 0.0f )
+		{
+			Camera.RotateDown( abs( CameraDiffs.y ) );
+		}
+		if( CameraDiffs.x > 0.0f  )
+		{
+			Camera.RotateRight( abs( CameraDiffs.x ) );
+		}
+		else if( CameraDiffs.x < 0.0f )
+		{
+			Camera.RotateLeft( abs( CameraDiffs.x ) );
+		}
+
+
+		// Clear the buffers
+		pGraphicDevice->ClearColor( );
+		pGraphicDevice->ClearDepth( );
+
+		// Bind the model shader program
+		pShaderProgram_Model->Bind( );
+
+		// Update the camera if needed
+		if( Camera.Update( DeltaTime ) )
+		{
+			pShaderProgram_Model->SetUniformMatrix4x4f( "ViewMatrix", Camera.GetMatrix( ) );
+		}
+
+		// Render the model
+		pLevelModel->Render( Bit::VertexObject::RenderMode_Triangles );
+
+		// Unbind the shader program
+		pShaderProgram_Model->Unbind( );
+
+
+		// Present the buffers
+		pGraphicDevice->Present( );
+	}
+
+	// We are done
+	bitTrace( "Closing the program.\n" );
+	return CloseApplication( 0 );
+}
+
+int CloseApplication( const int p_Code )
+{
+	// Release the resource manager
+	Bit::ResourceManager::Release( );
+
+	if( pLevelModel )
+	{
+		delete pLevelModel;
+		pLevelModel = BIT_NULL;
+	}
+
+	if( pShaderProgram_Model )
+	{
+		delete pShaderProgram_Model;
+		pShaderProgram_Model = BIT_NULL;
+	}
+
+	if( pVertexShader_Model )
+	{
+		delete pVertexShader_Model;
+		pVertexShader_Model = BIT_NULL;
+	}
+
+	if( pFragmentShader_Model )
+	{
+		delete pFragmentShader_Model;
+		pFragmentShader_Model = BIT_NULL;
+	}
+
+	if( pGraphicDevice )
+	{
+		delete pGraphicDevice;
+		pGraphicDevice = BIT_NULL;
+	}
+
+	if( pWindow )
+	{
+		delete pWindow;
+		pWindow = BIT_NULL;
+	}
+
+	// Return the code
+	return p_Code;
+}
+
+void InitializeMatrixManager( )
+{
+	// Projection
+	Bit::MatrixManager::SetMode( Bit::MatrixManager::Mode_Projection );
+	Bit::MatrixManager::LoadPerspective( 45.0f,(BIT_FLOAT32)WindowSize.x / (BIT_FLOAT32)WindowSize.y, 2.0f, 4000.0f ); 
+
+	// Model view
+	Bit::MatrixManager::SetMode( Bit::MatrixManager::Mode_View );
+	Bit::MatrixManager::LoadIdentity( );
+
+	Bit::MatrixManager::SetMode( Bit::MatrixManager::Mode_Model );
+	Bit::MatrixManager::LoadIdentity( );
+}
+
+void InitializeCamera( )
+{
+	Camera.SetPosition( Bit::Vector3_f32( -900.0f, 600.0f, -200.0f ) );
+	Camera.SetDirection( Bit::Vector3_f32( 1.0f, -0.5f, 0.4f ).Normal( ) );
+	Camera.SetMovementSpeed( 1000.0f );
+	Camera.SetEyeSpeed( 15.0f );
+	Camera.UpdateMatrix( );
+
+	// Set the matrix to the matrix manager
+	Bit::MatrixManager::SetMode( Bit::MatrixManager::Mode_View );
+	Bit::MatrixManager::SetMatrix( Camera.GetMatrix( ) );
+
+}
+
+BIT_UINT32 CreateWindow( )
+{
+	// Create a window
+	if( ( pWindow = Bit::CreateWindow( ) ) == BIT_NULL )
+	{
+		bitTrace( "[Error] Can not create the window\n" );
+		return BIT_ERROR;
+	}
+
+	// Create the style we want for the window
+	BIT_UINT32 Style = Bit::Window::Style_TitleBar | Bit::Window::Style_Minimize |
+		/* Bit::Window::Style_Resize | */ Bit::Window::Style_Close;
+
+	// Open the window
+	if( pWindow->Open( WindowSize, 32, "Hello World", Style ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not open the window\n" );
+		return BIT_ERROR;
+	}
+
+	// Calculate the mouse lock position
+	Bit::Vector2_si32 WindowPosition = pWindow->GetPosition( );
+	MouseLockPosition = WindowPosition + ( pWindow->GetSize( ) / 2 );
+
+	// Change the window's title
+	pWindow->SetTitle( "Cool. We can now change the window title. Testing swedish characters: åäö ÅÄÖ" );
+	pWindow->ShowCursor( BIT_FALSE );
+	pWindow->SetCursorPosition( MouseLockPosition );
+
+	return BIT_OK;
+}
+
+BIT_UINT32 CreateGraphicDevice( )
+{
+	// Create a graphic device
+	if( ( pGraphicDevice = Bit::CreateGraphicDevice( ) ) == BIT_NULL )
+	{
+		bitTrace( "[Error] Can not create the graphic device\n" );
+		return BIT_ERROR;
+	}
+
+	// Open the graphic device
+	if( pGraphicDevice->Open( *pWindow, 0 ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not open the graphic device\n" );
+		return BIT_ERROR;
+	}
+
+	// Set the clear color
+	pGraphicDevice->SetClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
+	pGraphicDevice->SetViewport( 0, 0, WindowSize.x, WindowSize.y );
+	pGraphicDevice->EnableDepthTest( );
+	pGraphicDevice->EnableTexture( );
+	pGraphicDevice->EnableAlpha( );
+	pGraphicDevice->EnableFaceCulling( Bit::GraphicDevice::Culling_BackFace );
+
+	// Initialize the resource manager
+	Bit::Texture::eFilter TextureFilters[ ] =
+	{
+		Bit::Texture::Filter_Min, Bit::Texture::Filter_Linear,
+		Bit::Texture::Filter_Mag, Bit::Texture::Filter_Linear,
+		Bit::Texture::Filter_Wrap_X, Bit::Texture::Filter_Repeat,
+		Bit::Texture::Filter_Wrap_Y, Bit::Texture::Filter_Repeat,
+		Bit::Texture::Filter_None, Bit::Texture::Filter_None
+	};
+
+	// Initialize the resource manager
+	if( Bit::ResourceManager::Initialize( pGraphicDevice, TextureFilters ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not initialize the resource manager\n" );
+		return BIT_ERROR;
+	}
+
+	return BIT_OK;
+}
+
+BIT_UINT32 CreateModel( )
+{
+	// Allocate the image
+	if( ( pLevelModel = pGraphicDevice->CreateModel( Bit::Model::Model_OBJ ) ) == BIT_NULL )
+	{
+		bitTrace( "[Error] Can not create the model\n" );
+	}
+
+	Bit::Timer Timer;
+	Timer.Start( );
+
+	BIT_UINT32 Status = BIT_OK;
+	if( ( Status = pLevelModel->ReadFile( Bit::GetAbsolutePath( LevelModelPath ).c_str( ) ) ) != BIT_OK )
+	{
+		if( Status == BIT_ERROR_OPEN_FILE )
+		{
+			bitTrace( "[Error] Can not open the file\n" );
+		}
+		else
+		{
+			bitTrace( "[Error] Can not read the model file\n" );
+		}
+
+		return BIT_ERROR;
+	}
+
+	// Load the model
+	BIT_UINT32 ModelVerteBits = Bit::VertexObject::Vertex_Position | Bit::VertexObject::Vertex_Texture
+		| Bit::VertexObject::Vertex_Normal | Bit::VertexObject::Vertex_Tangent | Bit::VertexObject::Vertex_Binormal;
+	Bit::Texture::eFilter TextureFilters[ ] =
+	{
+		Bit::Texture::Filter_Min, Bit::Texture::Filter_Linear_Mipmap,
+		Bit::Texture::Filter_Mag, Bit::Texture::Filter_Linear_Mipmap,
+		Bit::Texture::Filter_None, Bit::Texture::Filter_None
+	};
+	if( pLevelModel->Load( ModelVerteBits, TextureFilters, BIT_TRUE ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not load the model\n" );
+		return BIT_ERROR;
+	}
+
+	Timer.Stop( );
+	bitTrace( "Model load time: %f ms.\n", Timer.GetTime( ) * 1000.0f );
+
+	return BIT_OK;
+}
+
+BIT_UINT32 CreateModelShader( )
+{
+	// Shader sources
+	static const std::string VertexSource =
+		"#version 330 \n"
+		"precision highp float; \n"
+
+		"in vec3 Position; \n"
+		"in vec2 Texture; \n"
+		"in vec3 Normal; \n"
+		"in vec3 Tangent; \n"
+		"in vec3 Binormal; \n"
+
+		"out vec3 out_Position; \n"
+		"out vec2 out_Texture; \n"
+		"out vec3 out_Normal; \n"
+		"out vec3 out_Tangent; \n"
+		"out vec3 out_Binormal; \n"
+		"out mat3 out_TangentSpace; \n"
+
+		"uniform mat4 ProjectionMatrix; \n"
+		"uniform mat4 ViewMatrix; \n"
+
+		"void main(void) \n"
+		"{ \n"
+
+		// Set some out values
+		"	vec4 NewPosition = vec4( Position, 1.0 ); \n"
+		"	out_Position = NewPosition.xyz; \n"
+		"	out_Texture = Texture; \n"
+		"	out_Normal = normalize( Normal ); \n"
+		"	out_Tangent = normalize( Tangent ); \n"
+		"	out_Binormal = normalize( Binormal ); \n"
+
+		// Calculate the tangent space matrix
+		"	out_TangentSpace[ 0 ] = out_Tangent; \n"
+		"	out_TangentSpace[ 1 ] = out_Binormal; \n"
+		"	out_TangentSpace[ 2 ] = out_Normal; \n"
+
+		// Set the output position
+		"	gl_Position = ProjectionMatrix * ViewMatrix * NewPosition; \n"
+
+		"} \n";
+
+	static const std::string FragmentSource =
+		"#version 330 \n"
+		"precision highp float; \n"
+
+		"in vec3 out_Position; \n"
+		"in vec2 out_Texture; \n"
+		"in vec3 out_Normal; \n"
+		"in vec3 out_Tangent; \n"
+		"in vec3 out_Binormal; \n"
+		"in vec3 out_LightVec; \n"
+		"in mat3 out_TangentSpace; \n"
+
+		"out vec4 out_Color; \n"
+
+		"uniform sampler2D DiffuseTexture; \n"
+		"uniform sampler2D NormalTexture; \n"
+		"uniform vec3 LightPosition; \n"
+		"uniform int UseNormalMapping; \n"
+
+
+		"void main(void) \n"
+		"{ \n"
+
+		// Diffuse color map
+		"	vec4 DiffuseMap = texture2D( DiffuseTexture, out_Texture ); \n"
+		"	if( DiffuseMap.a == 0.0 ) { discard; } \n"
+
+		"	vec3 Light; \n"
+
+		// Compute the direction of the light source
+		"	vec3 LightDirection = normalize( vec3( LightPosition - out_Position ) ); \n"
+
+		// Are we using normal maps?
+		"	if( UseNormalMapping == 1 ) \n"
+		"	{ \n"
+				// Normal color map
+		"		vec4 NormalMap = texture2D( NormalTexture, out_Texture ); \n"
+		"		NormalMap.y = 1.0 - NormalMap.y; \n"
+		"		vec3 OldNormalDirection = 2.0 * NormalMap.rgb - 1.0; \n"
+
+		"		vec3 NormalDirection = normalize( out_TangentSpace * OldNormalDirection ); \n"
+
+		// Compute the light
+		"		Light = vec3( max( dot( LightDirection, NormalDirection ), 0.1 ) ); \n"
+
+		"	} \n"
+		// Use normal lighting
+		"	else \n"
+		"	{ \n"
+		"		Light = vec3( max( dot( LightDirection, out_Normal ), 0.1 ) ); \n"
+		"	} \n"
+
+		// Set the output color
+		"	out_Color = DiffuseMap * vec4( Light.xyz, 1.0 ); \n"
+		"} \n";
+
+	// Load the shaders
+	// Create the vertex and fragment shaders
+	if( ( pVertexShader_Model = pGraphicDevice->CreateShader( Bit::Shader::Vertex ) ) == BIT_NULL )
+	{
+		bitTrace( "[Error] Can not create the vertex shader\n" );
+		return BIT_ERROR;
+	}
+	if( ( pFragmentShader_Model = pGraphicDevice->CreateShader( Bit::Shader::Fragment ) ) == BIT_NULL )
+	{
+		bitTrace( "[Error] Can not create the vertex shader\n" );
+		return BIT_ERROR;
+	}
+
+	// Set the sources
+	pVertexShader_Model->SetSource( VertexSource );
+	pFragmentShader_Model->SetSource( FragmentSource );
+
+	// Compile the shaders
+	if( pVertexShader_Model->Compile( ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not compile the vertex shader\n" );
+		return BIT_ERROR;
+	}
+	if( pFragmentShader_Model->Compile( ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not compile the fragment shader\n" );
+		return BIT_ERROR;
+	}
+
+
+	// Create the shader program
+	if( ( pShaderProgram_Model = pGraphicDevice->CreateShaderProgram( ) ) == BIT_NULL )
+	{
+		bitTrace( "[Error] Can not create the shader program\n" );
+		return BIT_ERROR;
+	}
+
+	// Attach the shaders
+	if( pShaderProgram_Model->AttachShaders( pVertexShader_Model ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not attach the vertex shader\n" );
+		return BIT_ERROR;
+	}
+	if( pShaderProgram_Model->AttachShaders( pFragmentShader_Model ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not attach the fragment shader\n" );
+		return BIT_ERROR;
+	}
+
+	// Set attribute locations
+	pShaderProgram_Model->SetAttributeLocation( "Position", 0 );
+	pShaderProgram_Model->SetAttributeLocation( "Texture", 1 );
+	pShaderProgram_Model->SetAttributeLocation( "Normal", 2 );
+	pShaderProgram_Model->SetAttributeLocation( "Tangent", 3 );
+	pShaderProgram_Model->SetAttributeLocation( "Binormal", 4 );
+
+	// Link the shaders
+	if( pShaderProgram_Model->Link( ) != BIT_OK )
+	{
+		bitTrace( "[Error] Can not link the shader program\n" );
+		return BIT_ERROR;
+	}
+
+	// Set uniforms
+	pShaderProgram_Model->Bind( );
+	pShaderProgram_Model->SetUniform1i( "DiffuseTexture", 0 );
+	pShaderProgram_Model->SetUniform1i( "NormalTexture", 1 );
+	pShaderProgram_Model->SetUniformMatrix4x4f( "ProjectionMatrix",
+		Bit::MatrixManager::GetMatrix( Bit::MatrixManager::Mode_Projection ) );
+	pShaderProgram_Model->SetUniformMatrix4x4f( "ViewMatrix",
+		Bit::MatrixManager::GetMatrix( Bit::MatrixManager::Mode_View ) );
+	pShaderProgram_Model->SetUniform3f( "LightPosition", 1.0f, 100.0f, 0.0f );
+	pShaderProgram_Model->SetUniform1i( "UseNormalMapping", UseNormalMapping );
+	pShaderProgram_Model->Unbind( );
+	
+	return BIT_OK;
+}
